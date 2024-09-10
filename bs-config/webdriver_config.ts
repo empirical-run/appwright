71974dd8232a4bc1fdf9c57241433ab40c1c5ace
pushpam5 <pushpamsxn58@gmail.com>
2024-09-10 13:41:58 +0530

    chore: updated build version

--- bs-config/webdriver_config.ts
+++ bs-config/webdriver_config.ts
@@ -57,7 +57,7 @@ export const config = {
       deviceName: "Google Pixel 8",
       osVersion: "14.0",
       platformName: "android",
-      buildIdentifier: "342",
+      buildIdentifier: "344",
       buildName: "Leap Android build",
       sessionName: "Bstack Android Leap Tests",
     },
