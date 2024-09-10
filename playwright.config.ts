707d775fda13300e9ee5a8037c51ebb43a193aa5
pushpam5 <pushpamsxn58@gmail.com>
2024-09-10 13:40:30 +0530

    feat: added playwright utils for reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "@playwright/test";
+import { baseConfig } from "@empiricalrun/playwright-utils";
 
 export default defineConfig({
+  ...baseConfig,
   testDir: "leap-tests/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   timeout: 0,
-  reporter: [["json", { outputFile: "results.json" }], ["html"]],
   use: {
     trace: "on-first-retry",
   },
