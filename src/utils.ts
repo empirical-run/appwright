b9f5c3d85b22e37868be1d9573747b7a164ad785
Arjun Attam <arjunattam@gmail.com>
2024-09-25 12:29:17 +0530

    chore: move boxedStep to util and add locator docstrings (#44)

---
+++ src/utils.ts
@@ -0,0 +1,27 @@
+import test from "@playwright/test";
+
+export function boxedStep(
+  target: Function,
+  context: ClassMethodDecoratorContext,
+) {
+  return function replacementMethod(...args: any) {
+    //@ts-ignore
+    const path = this.path ? `("${this.path}")` : "";
+    const argsString = args.length
+      ? "(" +
+        Array.from(args)
+          .map((a) => JSON.stringify(a))
+          .join(" , ") +
+        ")"
+      : "";
+    const name = `${context.name as string}${path}${argsString}`;
+    return test.step(
+      name,
+      async () => {
+        // @ts-ignore
+        return await target.call(this, ...args);
+      },
+      { box: true },
+    ); // Note the "box" option here.
+  };
+}
