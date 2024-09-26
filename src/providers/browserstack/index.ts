8563e13fdace3b065027eed1ba7447e4980616b3
Arjun Attam <arjunattam@gmail.com>
2024-09-26 18:11:47 +0530

    chore: remove verbose logs (#67)

--- src/providers/browserstack/index.ts
+++ src/providers/browserstack/index.ts
@@ -93,8 +93,10 @@ export class BrowserStackDeviceProvider implements DeviceProvider {
       body,
     });
     const data = await response.json();
-    console.log("Upload response:", data);
     const appUrl = (data as any).app_url;
+    if (!appUrl) {
+      console.error("Uploading the build failed:", data);
+    }
     process.env[envVarKeyForBuild(this.project.name)] = appUrl;
   }
 
@@ -161,7 +163,6 @@ export class BrowserStackDeviceProvider implements DeviceProvider {
     );
     const dir = path.dirname(pathToTestVideo);
     fs.mkdirSync(dir, { recursive: true });
-    console.log(`Video URL: ${videoURL}`);
     /**
      * The BrowserStack video URL initially returns a 200 status,
      * but the video file may still be empty. To avoid downloading
