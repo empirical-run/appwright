27bbc13b4b5f654a66abaa4a449e7c0c63d98c62
pushpam5 <pushpamsxn58@gmail.com>
2024-09-27 16:37:26 +0530

    fix: handle multiple projects in config

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -3,12 +3,28 @@ import { AppwrightConfig } from "./types";
 import { createDeviceProvider } from "./providers";
 
 async function globalSetup(config: FullConfig<AppwrightConfig>) {
-  const length = config.projects.length;
-  for (let i = 0; i < length; i++) {
-    const project = config.projects[i]!;
-    const provider = createDeviceProvider(project);
-    await provider.globalSetup?.();
+  const projectIndex = process.argv.findIndex((arg) => arg === "--project");
+
+  const projectName =
+    projectIndex !== -1 ? process.argv[projectIndex + 1] : undefined;
+
+  console.log(`Using project: ${projectName}`);
+  if (!projectName) {
+    throw new Error(
+      `No project specified. Please pass --project <project-name>.`,
+    );
   }
+
+  const project = config.projects.find((p) => p.name === projectName);
+  if (!project) {
+    throw new Error(
+      `Project "${projectName}" does not match any project from the configuration.
+Please check the project name in appwright.config.ts.`,
+    );
+  }
+
+  const provider = createDeviceProvider(project);
+  await provider.globalSetup?.();
 }
 
 export default globalSetup;
