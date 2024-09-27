2a8bec8c9f21c33208f5b7eacbfc9200be2e639a
Saikat  Mitra <saikatmitra91@gmail.com>
2024-09-27 20:18:44 +0530

    feat: add support for test.skip if llm keys are not available

--- src/vision/index.ts
+++ src/vision/index.ts
@@ -2,6 +2,7 @@ import { getBoundingBox, query } from "@empiricalrun/llm/vision";
 // @ts-ignore ts not able to identify the import is just an interface
 import { Client as WebDriverClient } from "webdriver";
 import { Device } from "../device";
+import test from "@playwright/test";
 
 export interface AppwrightVision {
   extractText(prompt: string): Promise<string>;
@@ -15,11 +16,19 @@ export class VisionProvider {
   ) {}
 
   async extractText(prompt: string): Promise<string> {
+    test.skip(
+      !process.env.OPENAI_API_KEY,
+      "LLM vision based extract text is not enabled. Set the OPENAI_API_KEY environment variable to enable it",
+    );
     const base64Screenshot = await this.webDriverClient.takeScreenshot();
     return await query(base64Screenshot, prompt);
   }
 
   async tap(prompt: string): Promise<void> {
+    test.skip(
+      !process.env.GOOGLE_API_KEY,
+      "LLM vision based tap is not enabled. Set the GOOGLE_API_KEY environment variable to enable it",
+    );
     const base64Screenshot = await this.webDriverClient.takeScreenshot();
     const { center, container: imageSize } = await getBoundingBox(
       base64Screenshot,
