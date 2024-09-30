36d7fd7bced1e537f655a4fc2de671a8085c9ba8
pushpam5 <pushpamsxn58@gmail.com>
2024-09-30 20:07:33 +0530

    chore: added npm script to extract app file

--- example/appwright.config.ts
+++ example/appwright.config.ts
@@ -12,10 +12,11 @@ export default defineConfig({
       use: {
         platform: Platform.IOS,
         device: {
-          name: "iPhone 14 Pro",
           provider: "emulator",
+          name: "iPhone 14 Pro",
+          osVersion: "17.5",
         },
-        buildPath: join(process.cwd(), "Wikipedia.app"),
+        buildPath: join(process.cwd(), "wikipedia.app"),
       },
     },
     {
