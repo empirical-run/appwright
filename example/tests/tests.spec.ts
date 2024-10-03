cd3de10b6d5127edb97eecb909b2260ee933a129
Arjun Attam <arjunattam@gmail.com>
2024-10-03 13:47:58 +0530

    chore: clean readme and example

--- example/tests/tests.spec.ts
+++ example/tests/tests.spec.ts
@@ -1,13 +1,17 @@
 import { test, expect } from "appwright";
 
-test("Navigate to wikipedia dashboard, search for playwright and verify `Microsoft` is visible", async ({
+test("Open Playwright on Wikipedia and verify Microsoft is visible", async ({
   device,
 }) => {
+  // Dismiss splash screen
   await device.getByText("Skip").tap();
-  await device.getByText("Search Wikipedia").tap();
-  await device
-    .getByText("Search Wikipedia", {exact: true})
-    .fill("playwright");
+
+  // Enter search term
+  const searchInput = device.getByText("Search Wikipedia", { exact: true });
+  await searchInput.tap();
+  await searchInput.fill("playwright");
+
+  // Open search result and assert
   await device.getByText("Playwright (software)").tap();
   await expect(device.getByText("Microsoft")).toBeVisible();
 });
--- example/tools/extract.js
+++ example/tools/extract.js
@@ -1,20 +1,20 @@
 import { exec } from 'child_process';
-import  { promisify }  from 'util';
+import { promisify } from 'util';
 import fs from 'fs';
 
 const execPromise = promisify(exec);
 
-const zipFile = './wikipedia_ios.zip';
-const extractedFolder = './wikipedia_ios.app';
-const appFile = './wikipedia_ios.app/Wikipedia.app';
-const destinationApp = './wikipedia.app';
+const zipFile = './builds/wikipedia_ios.zip';
+const extractedFolder = './builds/wikipedia_ios.app';
+const appFile = './builds/wikipedia_ios.app/Wikipedia.app';
+const destinationApp = './builds/Wikipedia.app';
 
 async function extractApp() {
   try {
     await execPromise(`unzip -o ${zipFile} -d ${extractedFolder}`);
     await execPromise(`cp -r ${appFile} ${destinationApp}`);
     fs.rmSync(extractedFolder, { recursive: true });
-   } catch (error) {
+  } catch (error) {
     console.error(`extractApp: ${error.message}`);
   }
 }
