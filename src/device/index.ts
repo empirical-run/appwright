111a1f318821422080d718f6d689c3e562e20a84
Arjun Attam <arjunattam@gmail.com>
2024-10-20 20:01:33 +0530

    feat: alternate ios scroll implementation

--- src/device/index.ts
+++ src/device/index.ts
@@ -333,4 +333,13 @@ export class Device {
   async screenshot(): Promise<string> {
     return await this.webDriverClient.takeScreenshot();
   }
+
+  async scroll(): Promise<void> {
+    const driverSize = await this.webDriverClient.getWindowRect();
+    const from = { x: driverSize.width / 2, y: driverSize.height * 0.7 };
+    const to = { x: driverSize.width / 2, y: driverSize.height * 0.3 };
+    await this.webDriverClient.executeScript("mobile: dragFromToForDuration", [
+      { duration: 2, fromX: from.x, fromY: from.y, toX: to.x, toY: to.y },
+    ]);
+  }
 }
